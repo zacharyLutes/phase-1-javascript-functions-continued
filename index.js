497e8f03e33842e0a3919bb41156b2a627cba42e
forkingits1123 <lblack11235813@gmail.com>
2021-05-18 11:16:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(work="go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+
+const wrapAdjective = function(p='*') {
+    const adjective = function(parameter='special') {
+        return `You are ${p}${parameter}${p}`;
+    }
+    if (p === '*') {
+        return `You are ${p}a hard worker${p}!`;
+    } else if (p === '||') {
+        return `You are ${p}a dedicated programmer${p}!`;
+    }
+    return adjective;
+    
+}   
